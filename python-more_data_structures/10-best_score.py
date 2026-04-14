d629494d65bf2d57879224ef2e8b0a827bad53ba
Ayan1413 <13710@holbertonstudents.com>
2026-04-14 18:19:24 +0000

    Added task 10c

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
-    best_key = None
+     best_key = None
     max_value = 0
-    
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
             best_key = key
-            
     return best_key
