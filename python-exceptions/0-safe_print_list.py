8b4435bee1967abc5bf69e987b07c001927ef3cd
Ayan1413 <13710@holbertonstudents.com>
2026-04-16 19:15:38 +0000

    Added task 00

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    """
-    This module prints x elements of a list.
-    """
-    count = 0
+    idx = 0
 
-    # It iterates from 0 up to x.
-    for i in range(x):
-        try:
-            # attempting to access and print the element at index i.
-            # end="" ensures it prints on the same line.
-            print("{}".format(my_list[i]), end="")
+    try:
+        for i in my_list:
+            if idx < x:
+                print('{}'.format(my_list[idx]), end='')
+                idx += 1
 
-            # if the line above succeeded, it increments our counter.
-            count += 1
-
-        except IndexError:
-            # if my_list[i] doesn't exist, an IndexError happens.
-            # it catches it here and does nothing (pass) or break.
-            # this prevents the program from crashing.
-            pass
-
-    # printing a new line at the end of the output
-    print()
-
-    # returns the actual number of items it managed to print
-    return count
+        print()
+    except TypeError:
+        pass
+    finally:
+        return idx
