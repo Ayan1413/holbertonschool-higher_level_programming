9dfece3cdd725d4b56b7372504246306047d7d69
Ayan1413 <13710@holbertonstudents.com>
2026-04-16 18:59:38 +0000

    Added task 0aa

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    fir i in range(x):
-        try:
-print('{}'.format(my_list[i]), end='')
+    try:
+        while count < x:
+print("{}".format(my_list[count]), end="")
             count += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
     print("")
     return count
