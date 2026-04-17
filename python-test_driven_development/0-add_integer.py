241e7c975047424bdfbcc0853e1ec3e28734e2d1
Ayan1413 <13710@holbertonstudents.com>
2026-04-17 09:38:48 +0000

    Fix all issues

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-"""Modul add_integer funksiyasını təqdim edir."""
+"""
+This module provides the add_integer function.
+"""
 
 
 def add_integer(a, b=98):
-    """İki ədədi toplayır."""
+    """
+    Adds two integers. 
+    Args:
+        a (int, float): first number.
+        b (int, float): second number, defaults to 98.
+    Returns:
+        int: the sum.
+    Raises:
+        TypeError: if a or b are not integers or floats.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
