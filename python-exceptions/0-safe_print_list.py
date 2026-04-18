6bbc77334cd9073c20b65d554ba1f9328c024965
Ayan1413 <13710@holbertonstudents.com>
2026-04-18 08:00:05 +0000

    Fix safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for safe_print_list method."""
+
+
 def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list."""
     nb_print = 0
     for i in range(x):
         try:
