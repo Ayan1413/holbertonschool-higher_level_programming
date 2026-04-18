bc365ca27cd03a0605d9c61cd638e9981d9bffa4
Ayan1413 <13710@holbertonstudents.com>
2026-04-18 08:58:26 +0000

    Add task 5s

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,26 +8,20 @@ after each of these characters: '.', '?' and ':'.
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each '.', '?' and ':'.
-
     Args:
         text (str): The string to be formatted and printed.
-
     Raises:
         TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    # Flag to skip spaces at the start of a new line
     skip_space = True
-
     for char in text:
         if skip_space and char == ' ':
             continue
-
         print(char, end="")
         skip_space = False
-
         if char in ".?:":
-            print("\n")
+            print()
+            print()
             skip_space = True
