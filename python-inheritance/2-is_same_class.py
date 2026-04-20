4a569f2a5fab4b64636a2b6011f9866cd3bdb3f2
Ayan1413 <13710@holbertonstudents.com>
2026-04-20 12:13:55 +0000

    Fix pycodestyle errors

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-""" 
-Module for checking if an object is exactly an instance of a specified class.
+"""Module for checking if an object is exactly an instance of a class."""
 
-This module contains a function `is_same_class` that checks if a given object
-is exactly an instance of a specified class, and not an instance of a subclass.
-It helps in confirming the type of an object in situations where exact type matching
-is required.
-
-Functions:
-    is_same_class(obj, a_class): Returns True if obj is exactly an instance of a_class.
-"""
 
 def is_same_class(obj, a_class):
     """Tests if an object is exactly an instance of a specific class.
@@ -20,5 +11,6 @@ def is_same_class(obj, a_class):
 
     Returns:
         bool: True if obj is exactly an instance of a_class, False otherwise.
+
     """
-    return (type(obj) == a_class)
+    return type(obj) is a_class
