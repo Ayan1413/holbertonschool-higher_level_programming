fdd0c09f9533f57ed7178fb8fec3fd9a163c763c
Ayan1413 <13710@holbertonstudents.com>
2026-04-20 10:33:18 +0000

    Add task 0a

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-This module defines an empty class Square.
+This module contains a class Square that defines a square.
+The purpose of this module is to demonstrate a basic Python class.
 """
 
 
 class Square:
     """
-    An empty class that defines a square.
+    This is an empty class Square that defines a square.
+    It currently does not have any attributes or methods.
     """
     pass
