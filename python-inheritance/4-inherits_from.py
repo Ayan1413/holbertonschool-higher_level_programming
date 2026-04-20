8ee775c855459b5ad6e06ed0af312d237acfb853
Ayan1413 <13710@holbertonstudents.com>
2026-04-20 12:23:53 +0000

    Fix pycodestyle E721

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -15,6 +15,6 @@ def inherits_from(obj, a_class):
             False otherwise.
 
     """
-    if type(obj) == a_class:
-        return (False)
-    return (issubclass(type(obj), a_class))
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
