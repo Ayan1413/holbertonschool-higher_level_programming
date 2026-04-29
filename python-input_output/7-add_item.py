fad0dcaf82082040439b61a23af2c2265f91aa1d
Ayan1413 <13710@holbertonstudents.com>
2026-04-29 16:22:56 +0000

    Added task 7a

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """Python - Input/Output, task 9. Load, add, save  """
-
-
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 argv_edit = argv[1:]
-
 try:
     content_list = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     content_list = []
 finally:
     for arg in argv_edit:
