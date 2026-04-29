ec5906130b8627725dd7330667427e47844344c2
Ayan1413 <13710@holbertonstudents.com>
2026-04-29 16:13:24 +0000

    Added task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """
-Module for appending a string to a text file and returning the number of characters added.
+Module for appending a string to a text file and returning
+the number of characters added.
 
-This module contains a function `append_write` that appends a string to the specified
-file in UTF-8 encoding. If the file doesn't exist, it is created. The function returns
-the number of characters added to the file.
+This module contains a function `append_write` that appends a string
+to the specified file in UTF-8 encoding. If the file doesn't exist,
+it is created. The function returns the number of characters added.
 
 Functions:
-    append_write(filename="", text=""): Appends the string `text` to the file `filename`
-                                         and returns the number of characters added.
+    append_write(filename="", text=""): Appends the string `text`
+    to the file `filename` and returns the number of characters added.
 """
 
-def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF-8) and returns the number of characters added.
 
-    This function opens the specified file in append mode, which ensures that the string `text`
-    is added to the end of the file without modifying its existing content. If the file doesn't exist,
-    it is created. The function returns the number of characters added to the file.
+def append_write(filename="", text=""):
+    """Append a string to a text file (UTF-8) and return char count.
+
+    This function opens the specified file in append mode, which
+    ensures that the string `text` is added to the end of the file
+    without modifying its existing content. If the file doesn't
+    exist, it is created. The function returns the number of
+    characters added to the file.
 
     Args:
         filename (str): The name of the file to append to.
@@ -24,7 +28,7 @@ def append_write(filename="", text=""):
 
     Returns:
         int: The number of characters added to the file.
-    
+
     Example:
         nb_characters_added = append_write("file.txt", "Hello again!")
         print(nb_characters_added)
