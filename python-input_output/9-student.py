ba09dba9bf24bbb15edb00906cf0a0bdda1cb370
Ayan1413 <13710@holbertonstudents.com>
2026-04-29 16:30:41 +0000

    Added task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module to define a Student class."""
+
+
+class Student:
+    """A class to represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the public attributes.
+        """
+        return vars(self)
