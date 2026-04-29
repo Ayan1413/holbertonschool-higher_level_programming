0a888aafeeae08f6907b37930070cabcc4e01069
Ayan1413 <13710@holbertonstudents.com>
2026-04-29 16:17:19 +0000

    Added task 3ab

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ Python - Input/Output, task 5. To JSON string """
 import json
- 
- 
+
+
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (serialized string)
- 
+
     Args:
         my_obj (any): object to be serialized
     """
     return json.dumps(my_obj)
- 
-
