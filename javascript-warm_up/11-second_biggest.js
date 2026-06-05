05a09823e13c79d80e4b6ba24c9e97bd39c80e7f
Ayan1413 <13710@holbertonstudents.com>
2026-06-05 15:46:58 +0000

    Added task

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,10 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
-const nums = process.argv.slice(2).map(function (n) {
-  return parseInt(n);
-});
-const max = Math.max.apply(Math, nums);
-const min = Math.min.apply(Math, nums);
-
-if (len > 3) {
-  let i = 0;
-  let n = 0;
-  let secBig = min;
-
-  for (; i < len; ++i) {
-    n = nums[i];
-
-    if (n > secBig && n < max) {
-      secBig = n;
-    }
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
   }
-
-  console.log(secBig);
-} else {
-  console.log(0);
+  return n * factorial(n - 1);
 }
+
+console.log(factorial(parseInt(process.argv[2])));
